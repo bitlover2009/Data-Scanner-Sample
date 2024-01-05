9df11cd67b79870d5234ff0ce358f2132f81e083
bitlover2009 <bitlover2009@gmail.com>
2024-01-05 14:51:42 -0600

    Initial Commit

---
+++ Data Scanner Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Data Scanner Sample
+//
+//  Created by David Grau Beltrán  on 05/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Data Scanner Sample/Data_Scanner_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_Scanner_SampleApp.swift
+//  Data Scanner Sample
+//
+//  Created by David Grau Beltrán  on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Data_Scanner_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
